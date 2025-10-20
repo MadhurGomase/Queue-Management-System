cef73d05461a8c267f533ba96f1a2705814f5225
MadhurGomase <madhurgomase@gmail.com>
2025-10-20 23:34:43 +0530

    added addCustomer function to main function

--- qmsMain.cpp
+++ qmsMain.cpp
@@ -84,6 +84,50 @@ class Queue{
 };
 //queueInitialize.cpp ends
 
+//from addCustomer.cpp
+struct Customer{
+    string custName;
+    int taskChoice;
+    int arrivalTime;
+    int tokenNum;
+};
+
+int addCustomer(int choice){
+    Customer c;
+
+    cout << "Enter name: ";
+    getline(cin, c.custName);
+    c.taskChoice = choice;
+
+    string strChoice = to_string(choice);
+
+    //Fetch system time
+    time_t now = time(0);           // get current time
+    tm *ltm = localtime(&now);      // convert to local time
+
+    int hh = ltm->tm_hour;          // hours (0–23)
+    int mm = ltm->tm_min;           // minutes (0–59)
+
+    int timeArrival = hh * 100 + mm;       // combine into HHMM format
+    c.arrivalTime = timeArrival;
+
+    string strTime = to_string(timeArrival);
+
+    // Bank IFSC = XXXX0111111, here, 
+    //the last six digits indicate the
+    // bank branch location
+
+    string branchLoc = "111111";
+
+    //Token number will be a unique 
+    //temporary reference for the customer
+
+    c.tokenNum = stoi(strChoice + strTime);
+
+    return c.tokenNum;
+}
+//addCustomer.cpp ends
+
 int main(){
 
     int opt;
@@ -111,6 +155,7 @@ int main(){
             cout << "4. Foreign exchange\n";
             cout << "5. Other queries\n";
             cout << "Enter choice : ";
+
             cin >> task;
 
             
